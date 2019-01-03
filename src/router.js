5f85ea06af2125457b86676c51cd5867bbcef24e
沈飞 <shenfei@jiangduoduo.com>
2019-01-03 11:10:58 +0800

    fisrt commit

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import Home from "./views/Home.vue";
+import Login from "./views/login/login";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/home",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    }
+  ]
+});
