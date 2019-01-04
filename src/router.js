8c23a01377f0325e810ef2b43e9d22eb28e29c78
沈飞 <shenfei@jiangduoduo.com>
2019-01-04 11:45:03 +0800

    [+]前台页面 +router整理

--- src/router.js
+++ src/router.js
@@ -2,18 +2,24 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import store from './store';
 
-import Layout from './views/layout/layout';
-import Home from './views/home/home';
+import Houtai from './views/houtai/houtai';
+import Qiantai from './views/qiantai/qiantai';
+import index from './views/houtai/children/index/index';
 import Login from './views/login/login';
-import orderImport from './views/orderImport/orderImport';
-import systemEdit from './views/systemEdit/systemEdit';
+import orderImport from './views/houtai/children/orderImport/orderImport';
+import systemEdit from './views/houtai/children/systemEdit/systemEdit';
+
+import CurrentWipAreaOrder from './views/qiantai/children/currentWipAreaOrder/currentWipAreaOrder';
+import CurrentPullOrder from './views/qiantai/children/currentPullOrder/currentPullOrder';
+import TodayProOrder from './views/qiantai/children/todayProOrder/todayProOrder';
+import UnfinishOrder from './views/qiantai/children/unFinishOrder/unFinishOrder';
 
 Vue.use(Router);
 
-export const mainRouters = [{
-    path: 'home',
-    name: 'home',
-    component: Home,
+export const houtaiRouters = [{
+    path: 'index',
+    name: 'index',
+    component: index,
     meta: {
         title: '生产看板'
     }
@@ -35,6 +41,39 @@ export const mainRouters = [{
     }
 }
 ]
+export const qiantaiRouters = [{
+    path: 'currentWipAreaOrder',
+    name: 'currentWipAreaOrder',
+    component: CurrentWipAreaOrder,
+    meta: {
+        title: '当前Wip区订单'
+    }
+},
+{
+    path: 'currentPullOrder',
+    name: 'currentPullOrder',
+    component: CurrentPullOrder,
+    meta: {
+        title: '当前拉动订单'
+    }
+},
+{
+    path: 'todayProOrder',
+    name: 'todayProOrder',
+    component: TodayProOrder,
+    meta: {
+        title: '当前排产订单'
+    }
+},
+{
+    path: 'unfinishOrder',
+    name: 'unfinishOrder',
+    component: UnfinishOrder,
+    meta: {
+        title: '缺料装箱订单'
+    }
+}
+]
 
 const router = new Router({
     routes: [{
@@ -43,10 +82,16 @@ const router = new Router({
         component: Login
     },
     {
-        path: '/',
-        name: 'Layout',
-        component: Layout,
-        children:mainRouters
+        path: '/houtai/',
+        name: 'houtai',
+        component: Houtai,
+        children: houtaiRouters
+    },
+    {
+        path: '/qiantai/',
+        name: 'qiantai',
+        component: Qiantai,
+        children: qiantaiRouters
     }
     ]
 });
