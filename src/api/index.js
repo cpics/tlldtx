8e222a32842da8d8aa208e3cb428d3e5ae941b50
shenfei <shenfei@jiangduoduo.com>
2019-01-21 08:19:19 +0800

    1111

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import fetch from '../common/fetch/index';
-const domain = '//kone.2xnet.net:8023';
-// export const domain = location.host;
+// const domain = '//kone.2xnet.net:8023';
+export const domain = '//' + location.host;
 
 //用户登录
 export const login = params =>
