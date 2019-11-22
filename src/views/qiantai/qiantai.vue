6870fe985d4de08866c6bbe0909117703b7cedc3
shenfei <shenfei@moduoduodeMacBook-Pro.local>
2019-11-22 19:25:23 +0800

    【*】1111

--- src/views/qiantai/qiantai.vue
+++ src/views/qiantai/qiantai.vue
@@ -96,7 +96,7 @@ export default {
             qlCount: '',
             HistoryCount: [],
             qlHistoryCount: '',
-            leftSliderDir: true,
+            leftSliderDir: false,
 
         };
     },
